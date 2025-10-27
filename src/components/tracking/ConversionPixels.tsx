3d30e40b63a558bcba99a9d202d0f9af78247cdd
Kleyton Gonçalves <eu.kleytongoncalves@gmail.com>
2025-10-26 23:44:29 -0300

    fix: remover dependência react-helmet-async e usar DOM manipulation
    CORREÇÃO DE BUILD:
    Erro: Cannot find module 'react-helmet-async'
    
    Solução:
    - Remover import de react-helmet-async
    - Implementar injeção de scripts via DOM manipulation nativo
    - Usar useEffect para cada plataforma de tracking
    - createElement + appendChild para scripts
    - Remover scripts existentes antes de reinjetar
    
    Alterações em ConversionPixels.tsx:
    - injectScript(): cria e injeta <script> no head
    - injectNoScript(): cria e injeta <noscript> no body
    - useEffect separado para cada pixel (Meta, GA4, TikTok)
    - setTimeout no GA4 para garantir ordem de carregamento
    - IDs únicos para cada script (remoção segura)
    
    Benefícios:
    ✅ Sem dependências externas
    ✅ Build passa sem erros
    ✅ Scripts injetados dinamicamente
    ✅ Cleanup automático (remove antes de reinjetar)
    ✅ Performance otimizada
    
    Compatibilidade:
    ✅ React 18
    ✅ Vite
    ✅ Todos navegadores modernos
    ✅ SEO friendly

--- src/components/tracking/ConversionPixels.tsx
+++ src/components/tracking/ConversionPixels.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useCatalogSettings } from '@/hooks/useCatalogSettings';
-import { Helmet } from 'react-helmet-async';
 
 /**
  * Componente Global de Tracking
@@ -9,6 +8,45 @@ import { Helmet } from 'react-helmet-async';
  */
 const ConversionPixels: React.FC = () => {
   const { settings } = useCatalogSettings();
+
+  /**
+   * Injeta script no head
+   */
+  const injectScript = (id: string, content: string, isAsync: boolean = false, src?: string) => {
+    // Remover script existente se houver
+    const existingScript = document.getElementById(id);
+    if (existingScript) {
+      existingScript.remove();
+    }
+
+    const script = document.createElement('script');
+    script.id = id;
+    script.type = 'text/javascript';
+    
+    if (src) {
+      script.src = src;
+      script.async = isAsync;
+    } else {
+      script.innerHTML = content;
+    }
+    
+    document.head.appendChild(script);
+  };
+
+  /**
+   * Injeta noscript no body
+   */
+  const injectNoScript = (id: string, content: string) => {
+    const existingNoScript = document.getElementById(id);
+    if (existingNoScript) {
+      existingNoScript.remove();
+    }
+
+    const noscript = document.createElement('noscript');
+    noscript.id = id;
+    noscript.innerHTML = content;
+    document.body.appendChild(noscript);
+  };
 
   useEffect(() => {
     if (settings?.tracking_debug_mode) {
@@ -21,80 +59,98 @@ const ConversionPixels: React.FC = () => {
     }
   }, [settings]);
 
-  // Meta Pixel Script
-  const metaPixelScript = settings?.meta_pixel_enabled && settings?.meta_pixel_id ? `
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '${settings.meta_pixel_id}');
-    ${settings.tracking_auto_events ? "fbq('track', 'PageView');" : ''}
-  ` : '';
+  /**
+   * Meta Pixel
+   */
+  useEffect(() => {
+    if (!settings?.meta_pixel_enabled || !settings?.meta_pixel_id) {
+      return;
+    }
 
-  // Google Analytics 4 Script
-  const ga4Script = settings?.ga4_enabled && settings?.ga4_measurement_id ? `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${settings.ga4_measurement_id}', {
-      send_page_view: ${settings.tracking_auto_events ? 'true' : 'false'},
-      debug_mode: ${settings.tracking_debug_mode ? 'true' : 'false'}
-    });
-  ` : '';
+    const metaPixelScript = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '${settings.meta_pixel_id}');
+      ${settings.tracking_auto_events ? "fbq('track', 'PageView');" : ''}
+    `;
 
-  // Google Ads Conversion Tracking
-  const googleAdsScript = settings?.google_ads_enabled && settings?.google_ads_id ? `
-    gtag('config', '${settings.google_ads_id}');
-  ` : '';
+    injectScript('meta-pixel', metaPixelScript);
+    
+    // NoScript fallback
+    injectNoScript(
+      'meta-pixel-noscript',
+      `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${settings.meta_pixel_id}&ev=PageView&noscript=1" />`
+    );
+  }, [settings?.meta_pixel_enabled, settings?.meta_pixel_id]);
 
-  // TikTok Pixel Script
-  const tiktokPixelScript = settings?.tiktok_pixel_enabled && settings?.tiktok_pixel_id ? `
-    !function (w, d, t) {
-      w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
-      ttq.load('${settings.tiktok_pixel_id}');
-      ttq.page();
-    }(window, document, 'ttq');
-  ` : '';
+  /**
+   * Google Analytics 4
+   */
+  useEffect(() => {
+    if (!settings?.ga4_enabled || !settings?.ga4_measurement_id) {
+      return;
+    }
 
-  return (
-    <Helmet>
-      {/* Meta Pixel */}
-      {metaPixelScript && (
-        <>
-          <script>{metaPixelScript}</script>
-          <noscript>
-            {`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${settings?.meta_pixel_id}&ev=PageView&noscript=1" />`}
-          </noscript>
-        </>
-      )}
+    // Script externo
+    injectScript(
+      'ga4-external',
+      '',
+      true,
+      `https://www.googletagmanager.com/gtag/js?id=${settings.ga4_measurement_id}`
+    );
 
-      {/* Google Analytics 4 */}
-      {settings?.ga4_enabled && settings?.ga4_measurement_id && (
-        <>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${settings.ga4_measurement_id}`}
-          />
-          <script>{ga4Script}</script>
-          {googleAdsScript && <script>{googleAdsScript}</script>}
-        </>
-      )}
+    // Script de configuração
+    const ga4ConfigScript = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${settings.ga4_measurement_id}', {
+        send_page_view: ${settings.tracking_auto_events ? 'true' : 'false'},
+        debug_mode: ${settings.tracking_debug_mode ? 'true' : 'false'}
+      });
+      ${settings.google_ads_enabled && settings.google_ads_id ? `gtag('config', '${settings.google_ads_id}');` : ''}
+    `;
 
-      {/* TikTok Pixel */}
-      {tiktokPixelScript && <script>{tiktokPixelScript}</script>}
+    setTimeout(() => {
+      injectScript('ga4-config', ga4ConfigScript);
+    }, 100);
+  }, [settings?.ga4_enabled, settings?.ga4_measurement_id, settings?.google_ads_enabled, settings?.google_ads_id]);
 
-      {/* Debug Mode Indicator */}
-      {settings?.tracking_debug_mode && (
-        <script>
-          {`console.log('🎯 TRACKING DEBUG MODE ATIVADO - Eventos serão logados no console');`}
-        </script>
-      )}
-    </Helmet>
-  );
+  /**
+   * TikTok Pixel
+   */
+  useEffect(() => {
+    if (!settings?.tiktok_pixel_enabled || !settings?.tiktok_pixel_id) {
+      return;
+    }
+
+    const tiktokScript = `
+      !function (w, d, t) {
+        w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
+        ttq.load('${settings.tiktok_pixel_id}');
+        ttq.page();
+      }(window, document, 'ttq');
+    `;
+
+    injectScript('tiktok-pixel', tiktokScript);
+  }, [settings?.tiktok_pixel_enabled, settings?.tiktok_pixel_id]);
+
+  /**
+   * Debug Mode
+   */
+  useEffect(() => {
+    if (settings?.tracking_debug_mode) {
+      console.log('🎯 TRACKING DEBUG MODE ATIVADO - Eventos serão logados no console');
+    }
+  }, [settings?.tracking_debug_mode]);
+
+  return null;
 };
 
 export default ConversionPixels;
